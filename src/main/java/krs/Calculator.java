101ef8ff18010f7372a5caba7fdc64bedd0cf401
rokhaya69 <54942212+rokhaya69@users.noreply.github.com>
2020-11-29 19:06:45 +0000

    Initial commit

---
+++ src/main/java/krs/Calculator.java
@@ -0,0 +1,43 @@
+package krs;
+
+import java.util.Arrays;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int min(int a, int b) {
+        return Math.min(a, b);
+    }
+
+    public int max(int a, int b) {
+        return Math.max(a, b);
+    }
+
+    public int minElement(int [] list){
+        Arrays.sort(list);
+        return list[0];
+    }
+
+    
+    public int maxElement(int [] list)  {
+        Arrays.sort(list);
+        return list[list.length-1];
+
+
+    }
+}
